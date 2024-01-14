08f2535ac182bef99ea6d2d9842a9120fcbbea76
SharfarazMahmood <sharfarazmahmood@gmail.com>
2024-01-14 13:48:21 +0600

    bkash demo added.

--- BkashPayment/Controllers/HomeController.cs
+++ BkashPayment/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace BkashPayment.Controllers
             return View();
         }
 
+        public IActionResult CheckoutSuccess()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
