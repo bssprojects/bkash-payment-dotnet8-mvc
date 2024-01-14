99bc376be287aeea76281522c4be85aa41217e1e
Sharfaraz Mahmood <sharfaraz@xinaps.com>
2024-01-14 12:57:02 +0600

    Project setup

---
+++ BkashPayment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BkashPayment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
